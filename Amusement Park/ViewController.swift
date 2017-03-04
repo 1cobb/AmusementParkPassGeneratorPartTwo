02832132b9ff3385dc80841b537380989158f277
1cobb <jam.cobb@icloud.com>
2017-03-04 10:10:50 -0500

    test cases

--- Amusement Park/ViewController.swift
+++ Amusement Park/ViewController.swift
@@ -14,9 +14,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         //Attendee Input
-        let attendee = Person(firstName: "James", lastName: "Cobb", streetAddress: "123 South Farm Street", city: "Atlanta", state: "Georgia", zipCode: 30253, dateOfBirth: "9/13/1992")
-        
+        let attendee = Person(firstName: "James", lastName: "Cobb", streetAddress: "123 South Farm Street", city: "Atlanta", state: "Georgia", zipCode: 30306, dateOfBirth: "9/13/1992")
         let parkPass = PassGenerator(attendee: attendee, typeOfAttendee: Employees.manager)
+        
+        /* let attendee = Person(firstName: "Juan", lastName: "Rodriguiz", streetAddress: "125 Ford St", city: "McDonough", state: "Georgia", zipCode: 30253, dateOfBirth: "3/15/1990")
+        let parkPass = PassGenerator(attendee: attendee, typeOfAttendee: Employees.hourlyFoodServices) */
+        
+        /* let attendee = Person(firstName: "Jimmy", lastName: "Johnson", streetAddress: "345 Spring St", city: "Lawrenceville", state: "Georgia", zipCode: 30044, dateOfBirth: "5/2/1996")
+        let parkPass = PassGenerator(attendee: attendee, typeOfAttendee: Employees.hourlyRideServices) */
+        
+        /* let attendee = Person(firstName: "Tammy", lastName: "Foster", streetAddress: "555 Derring Rd", city: "College Park", state: "Georgia", zipCode: 30456, dateOfBirth: "3/12/1989")
+        let parkPass = PassGenerator(attendee: attendee, typeOfAttendee: Employees.hourlyMaintenace) */
+        
+        /* let attendee = Person(firstName: "Rosa", lastName: "Carter", streetAddress: "213 Ring St", city: "Atlanta", state: "Georgia", zipCode: 30305, dateOfBirth: "4/23/1985")
+        let parkPass = PassGenerator(attendee: attendee, typeOfAttendee: Guest.classicGuest) */
+        
+        /* let attendee = Person(firstName: "Cameron", lastName: "Giles", streetAddress: "455 Park Lane", city: "Buckhead", state: "Georgia", zipCode: 30324, dateOfBirth: "4/5/2005")
+        let parkPass = PassGenerator(attendee: attendee, typeOfAttendee: Guest.freeChild) */
+        
+        /* let attendee = Person(firstName: "Joy", lastName: "Giles", streetAddress: "455 Park lane", city: "Buckhead", state: "Georgia", zipCode: 30324, dateOfBirth: "3/4/1980")
+        let parkPass = PassGenerator(attendee: attendee, typeOfAttendee: Guest.vip) */
         
         parkPass.AttendeePass()
         do {
